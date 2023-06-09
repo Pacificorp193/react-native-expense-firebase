2366902e7a03d01fca687359de5b5e8170f68743
Pacificorp193 <54546134+Pacificorp193@users.noreply.github.com>
2023-06-09 10:18:33 -0600

    Update date.js

--- util/date.js
+++ util/date.js
@@ -1,7 +1,7 @@
 export function getFormattedDate(date) {
-  return date.toISOString().slice(0, 10);
+  return date.toLocaleDateString();
 }
 
 export function getDateMinusDays(date, days) {
-  return new Date(date.getFullYear(), date.getMonth(), date.getDate() - days);
+  return new Date(date.getMonth(), date.getDate(), date.getFullYear() - days);
 }
