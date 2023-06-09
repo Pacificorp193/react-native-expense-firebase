4868e1f0e46269774bd7ecc7633e28c715c0b75f
Pacificorp193 <54546134+Pacificorp193@users.noreply.github.com>
2023-06-09 10:11:14 -0600

    Update styles.js

--- constants/styles.js
+++ constants/styles.js
@@ -1,15 +1,15 @@
 export const GlobalStyles = {
   colors: {
-    primary50: '#FF6347',
+    primary50: '#FFFFFF',
     primary100: '#FD1C03',
     primary200: '#FF2400',
-    primary400: '#E41B17',
+    primary400: '#000000',
     primary500: '#C11B17',
     primary700: '#990012',
     primary800: '#8C001A',
     accent500: '#f7bc0c',
     error50: '#F08080',
-    error500: '#9F000F',
+    error500: '#FFFFFF',
     gray500: '#39324a',
     gray700: '#221c30',
   },
